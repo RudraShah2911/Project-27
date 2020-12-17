d7d5dd7a0b7afa4fce941cb4ae8ba76ecc268765
RudraShah2911 <73107269+RudraShah2911@users.noreply.github.com>
2020-12-17 15:46:37 +0530

    Add files via upload

---
+++ Bob.js
@@ -0,0 +1,28 @@
+class Bob  {
+
+    constructor(x,y,r){
+        var ops = {
+            isStatic : false,
+            restitution : 1,
+            friction : 0.5,
+            density : 0.5
+        }
+        
+        this.x = x
+        this.y = y
+        this.r = r
+        this.body = Bodies.circle(this.x, this.y, this.r, ops)
+        World.add(world, this.body)
+    }
+    display(){
+        var bobpos = this.body.position
+
+        push()
+        translate(bobpos.x, bobpos.y)
+        ellipseMode(RADIUS)
+        strokeWeight(1)
+        fill(255,0,255)
+        ellipse(0,0,this.r,this.r)
+        pop()
+    }
+}
